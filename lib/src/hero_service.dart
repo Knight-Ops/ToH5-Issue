dc1e98f9586fc538b4dc51c0b2225eafd9e2f06a
Carl Hurd <carl@basilisklabs.com>
2018-05-01 18:50:19 -0400

    Problem Commit

---
+++ lib/src/hero_service.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+import 'dart:async';
+
+import 'hero.dart';
+import 'mock_heroes.dart';
+
+@Injectable()
+class HeroService {
+  Future<List<Hero>> getAll() async => mockHeroes;
+
+  Future<List<Hero>> getAllSlowly() {
+    return new Future.delayed(const Duration(seconds: 2), getAll);
+  }
+}
